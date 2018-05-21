4a8ff6b380192b27ac290fcc1d207599f647cf00
Colton Weaver <coltonbweaver@gmail.com>
2018-05-21 14:54:12 -0500

    local-wallpaper-analyzer.py now downloads the files from reddit and saves them to an unprocessed images bucket in S3

--- local-wallpaper-analyzer.py
+++ local-wallpaper-analyzer.py
@@ -1,18 +1,50 @@
 # This file is intended to test the logic that will be used in the lambda function
 
+import boto3
 import requests
+import os
 
 SUBREDDIT = "wallpapers"
+UNPROCESSED_BUCKET = "cwunprocessedimages"
+PROCESSED_BUCKET = "cwprocessedimages"
 
 if __name__ == '__main__':
 
-    r = requests.get("https://www.reddit.com/r/%s/top/.json" % SUBREDDIT).json()
+    s3 = boto3.resource('s3')
+    sub_top_posts = requests.get("https://www.reddit.com/r/%s/top/.json" % SUBREDDIT).json()
+    i = 0
 
-    print("Here is what was retrieved:")
+    for post in sub_top_posts["data"]["children"]:
 
-    for json_child in r["data"]["children"]:
+        url = post["data"]["url"]
 
-        print(json_child["data"]["url"])
+        if "i.redd.it" in url:
+            """
+
+            If the link is a i.redd.it link then the image is downloaded and uploaded to an S3 bucket for unprocessed images.
+            The files are saved locally and then deleted.
+
+            """    
+
+            file_name = "pic" + str(i) + ".jpg"
+            with open(file_name,'wb') as f:
+                pic = requests.get(url, stream=True)
+
+                for block in pic.iter_content(1024):
+                    if not block:
+                        break
+
+                    f.write(block)
+            
+            data = open(file_name, "rb")
+            s3.Bucket('cwunprocessedimages').put_object(Key=file_name, Body=data)
+            data.close()
+
+            os.remove(file_name)
+
+            i = i + 1
+
+    
 
 
 
